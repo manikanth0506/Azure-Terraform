a7c0387dee21094acd71964f804db5426e41b9b9
manikanth0506 <manishalovely14@gmail.com>
2022-09-17 17:55:19 +0530

    added two subnets

--- remote-state.tf
+++ remote-state.tf
@@ -1,15 +1,15 @@
 terraform {
-#   required_providers {
-#     azurerm = {
-#       source  = "hashicorp/azurerm"
-#       version = "=2.46.0"
-#     }
-#   }
-    backend "azurerm" {
-        resource_group_name  = "Terraform-State"
-        storage_account_name = "statetfterraform"
-        container_name       = "tfstate"
-        key                  = "terraform.tfstate"
-    }
+  #   required_providers {
+  #     azurerm = {
+  #       source  = "hashicorp/azurerm"
+  #       version = "=2.46.0"
+  #     }
+  #   }
+  backend "azurerm" {
+    resource_group_name  = "Terraform-State"
+    storage_account_name = "statetfterraform"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 
 }
--- virtualnetwork.tf
+++ virtualnetwork.tf
@@ -10,3 +10,17 @@ resource "azurerm_virtual_network" "Virtual-Network" {
     environment = "Development"
   }
 }
+
+resource "azurerm_subnet" "subnet-1" {
+  name                 = "subnet-1"
+  resource_group_name  = azurerm_resource_group.DevopsRG.name
+  virtual_network_name = azurerm_virtual_network.Virtual-Network.name
+  address_prefixes     = ["10.28.1.0/24"]
+}
+
+resource "azurerm_subnet" "subnet-2" {
+  name                 = "subnet-2"
+  resource_group_name  = azurerm_resource_group.DevopsRG.name
+  virtual_network_name = azurerm_virtual_network.Virtual-Network.name
+  address_prefixes     = ["10.28.2.0/24"]
+}
