2071eb03645abbebb6562452884c7ab26107bad1
manikanth0506 <manishalovely14@gmail.com>
2022-09-18 15:51:20 +0530

    modified provider

--- Provider.tf
+++ Provider.tf
@@ -14,3 +14,10 @@ provider "azurerm" {
 # $env:ARM_SUBSCRIPTION_ID="7c401650-777c-4303-a02d-f6fec53a588e"
 # $env:ARM_TENANT_ID="24c249db-c3ad-4ccc-a130-a302d59ab12a"
 # $env:ARM_CLIENT_SECRET="KuI8Q~xlQE3Zb6mJUVmTrNqtaOwDv-2.4nT91aON"
+
+
+
+# export ARM_CLIENT_ID="399f8994-8c78-4d94-8074-a4919cb62e82"
+# export ARM_SUBSCRIPTION_ID="7c401650-777c-4303-a02d-f6fec53a588e"
+# export ARM_TENANT_ID="24c249db-c3ad-4ccc-a130-a302d59ab12a"
+# export ARM_CLIENT_SECRET="KuI8Q~xlQE3Zb6mJUVmTrNqtaOwDv-2.4nT91aON"
