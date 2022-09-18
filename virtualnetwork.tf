c4b4e4ac5d083c89fd333b822c98a57b951b555b
root <root@ip-10-30-1-187.ec2.internal>
2022-09-18 10:09:56 +0000

    Added subnets 3,4

--- virtualnetwork.tf
+++ virtualnetwork.tf
@@ -1,4 +1,5 @@
 resource "azurerm_virtual_network" "Virtual-Network" {
+
   name                = "Virtual-Network"
   location            = azurerm_resource_group.DevopsRG.location
   resource_group_name = azurerm_resource_group.DevopsRG.name
@@ -23,4 +24,18 @@ resource "azurerm_subnet" "subnet-2" {
   resource_group_name  = azurerm_resource_group.DevopsRG.name
   virtual_network_name = azurerm_virtual_network.Virtual-Network.name
   address_prefixes     = ["10.28.2.0/24"]
+}
+
+resource "azurerm_subnet" "subnet-3" {
+  name                 = "subnet-3"
+  resource_group_name  = azurerm_resource_group.DevopsRG.name
+  virtual_network_name = azurerm_virtual_network.Virtual-Network.name
+  address_prefixes     = ["10.29.1.0/24"]
+}
+
+resource "azurerm_subnet" "subnet-4" {
+  name                 = "subnet-4"
+  resource_group_name  = azurerm_resource_group.DevopsRG.name
+  virtual_network_name = azurerm_virtual_network.Virtual-Network.name
+  address_prefixes     = ["10.29.2.0/24"]
 }
