143bbd28471be97cd4107dad0550d62d9e5c2024
manikanth0506 <manishalovely14@gmail.com>
2022-09-19 22:00:28 +0530

    created storageacnt1

--- Provider.tf
+++ Provider.tf
@@ -1,10 +1,10 @@
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
-  #   subscription_id = "7c401650-777c-4303-a02d-f6fec53a588e"
-  #   client_id       = "399f8994-8c78-4d94-8074-a4919cb62e82"
-  #   client_secret   = "KuI8Q~xlQE3Zb6mJUVmTrNqtaOwDv-2.4nT91aON"
-  #   tenant_id       = "24c249db-c3ad-4ccc-a130-a302d59ab12a"
+  # subscription_id = "7c401650-777c-4303-a02d-f6fec53a588e"
+  # client_id       = "399f8994-8c78-4d94-8074-a4919cb62e82"
+  # client_secret   = "KuI8Q~xlQE3Zb6mJUVmTrNqtaOwDv-2.4nT91aON"
+  # tenant_id       = "24c249db-c3ad-4ccc-a130-a302d59ab12a"
 
 }
 
