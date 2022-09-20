c92edfcbd5b7c89ede986a7df902c759541421fc
manikanth0506 <manishalovely14@gmail.com>
2022-09-20 23:31:47 +0530

    Added test2&3 records in dns.tf

--- Dns.tf
+++ Dns.tf
@@ -10,3 +10,19 @@ resource "azurerm_dns_a_record" "Testserver1" {
   ttl                 = 300
   records             = ["10.1.1.100"]
 }
+
+resource "azurerm_dns_a_record" "Testserver2" {
+  name                = "Testserver2"
+  zone_name           = azurerm_dns_zone.Devopscom.name
+  resource_group_name = azurerm_resource_group.DevopsRG.name
+  ttl                 = 300
+  records             = ["10.1.1.101"]
+}
+
+resource "azurerm_dns_a_record" "Testserver3" {
+  name                = "Testserver3"
+  zone_name           = azurerm_dns_zone.Devopscom.name
+  resource_group_name = azurerm_resource_group.DevopsRG.name
+  ttl                 = 300
+  records             = ["10.1.1.102"]
+}
