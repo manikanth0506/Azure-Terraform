1a08b821bd892416370b10168a93923ad4e1831f
manikanth0506 <manishalovely14@gmail.com>
2022-09-20 23:17:21 +0530

    Added dns  record in Dns.tf

--- Dns.tf
+++ Dns.tf
@@ -2,3 +2,11 @@ resource "azurerm_dns_zone" "Devopscom" {
   name                = "Devopscom"
   resource_group_name = azurerm_resource_group.DevopsRG.name
 }
+
+resource "azurerm_dns_a_record" "Testserver1" {
+  name                = "Testserver1"
+  zone_name           = azurerm_dns_zone.Devopscom.name
+  resource_group_name = azurerm_resource_group.DevopsRG.name
+  ttl                 = 300
+  records             = ["10.1.1.100"]
+}
