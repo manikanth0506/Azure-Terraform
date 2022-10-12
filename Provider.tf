0653f60f487fc40c956821d42f357a0c970f0895
manikanth0506 <manishalovely14@gmail.com>
2022-10-12 17:35:20 +0530

    added some changes

--- Provider.tf
+++ Provider.tf
@@ -2,22 +2,22 @@
 provider "azurerm" {
   features {}
   # subscription_id = "7c401650-777c-4303-a02d-f6fec53a588e"
-  # client_id       = "399f8994-8c78-4d94-8074-a4919cb62e82"
-  # client_secret   = "xhR8Q~Kswd~IZ42PRUkRP_Smtb5SNEwvN5dW9c3l"
+  # client_id       = "8564d7b7-4e6e-4cc3-a775-fe2415abac13"
+  # client_secret   = "JX38Q~tAGcdp4Ts6ylTrVycco39qJzfVRwgPAbC0"
   # tenant_id       = "24c249db-c3ad-4ccc-a130-a302d59ab12a"
 
 }
 
 # env declaration in power shell
 
-# $env:ARM_CLIENT_ID="399f8994-8c78-4d94-8074-a4919cb62e82"
+# $env:ARM_CLIENT_ID="8564d7b7-4e6e-4cc3-a775-fe2415abac13"
 # $env:ARM_SUBSCRIPTION_ID="7c401650-777c-4303-a02d-f6fec53a588e"
 # $env:ARM_TENANT_ID="24c249db-c3ad-4ccc-a130-a302d59ab12a"
-# $env:ARM_CLIENT_SECRET="xhR8Q~Kswd~IZ42PRUkRP_Smtb5SNEwvN5dW9c3l"
+# $env:ARM_CLIENT_SECRET="JX38Q~tAGcdp4Ts6ylTrVycco39qJzfVRwgPAbC0"
 
 
 
-# export ARM_CLIENT_ID="399f8994-8c78-4d94-8074-a4919cb62e82"
+# export ARM_CLIENT_ID="8564d7b7-4e6e-4cc3-a775-fe2415abac13"
 # export ARM_SUBSCRIPTION_ID="7c401650-777c-4303-a02d-f6fec53a588e"
 # export ARM_TENANT_ID="24c249db-c3ad-4ccc-a130-a302d59ab12a"
-# export ARM_CLIENT_SECRET="xhR8Q~Kswd~IZ42PRUkRP_Smtb5SNEwvN5dW9c3l"
+# export ARM_CLIENT_SECRET="JX38Q~tAGcdp4Ts6ylTrVycco39qJzfVRwgPAbC0"
